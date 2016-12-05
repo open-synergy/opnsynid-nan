ddd1ab491f730dcadbcfd5e1704cd6a21f7cc1bf
mikevhe18 <viriyananda.michael@gmail.com>
2016-12-05 18:30:21 +0700

    Adding module nan_purchase_additional_note (#7)
    * Adding module nan_purchase_additional_note
    
    Minor Updates
    
    * Update views

---
+++ nan_purchase_additional_note/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Natura - Purchase Additional Note",
+    "version": "8.0.1.0.0",
+    "category": "Purchase",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["purchase"],
+    "data": ["views/purchase_order_view.xml"],
+}
